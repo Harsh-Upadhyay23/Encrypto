6c90829063a2615fa5fa635974b2b8521c77f719
Harsh Upadhyay <upadhyayharsh435@gmail.com>
2025-08-31 20:39:42 +0530

    done

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -3,13 +3,17 @@ import { useState } from "react";
 
 const Manager = () => {
     const [show, setShow] = useState(false);
-    const [form, setForm] = useState({ site: "", username: "", password: "" });
+    const [form, setform] = useState({ site: "", username: "", password: "" });
 
     const showpassword = () => {
         setShow((prev) => !prev);
     };
 
     const savePassword = () => {
+
+    }
+    const handleChange=(e)=>{
+        setform({...form,[e.target.name]:e.target.value});
 
     }
 
@@ -32,8 +36,9 @@ const Manager = () => {
 
                     {/* Compact Form Row */}
                     <input
-                        type="text" onchange={handleChange}
+                        type="text" onChange={e=>handleChange(e)}
                         value={form.site}
+                        name="site"
                         placeholder="Enter website URL"
                         className="flex-1 px-1  rounded-md bg-blue-50 border border-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-300 placeholder-gray-400 text-sm
               w-full py-2 my-1"
@@ -42,18 +47,21 @@ const Manager = () => {
 
                         <input 
                             type="text"
-                            value={form.username} onchange={handleChange}
+                            name="username"
+                            value={form.username}
+                            onChange={e=>handleChange(e)}
                             placeholder="Enter username"
                             className=" px-3 py-2 rounded-md bg-green-50 border border-green-200 focus:outline-none focus:ring-2 focus:ring-green-300 placeholder-gray-400 text-sm"
                         />
                         <div className="relative ">
                             <input 
-                                value={form.password} onchange={handleChange}
+                                name="password"
+                                value={form.password} onChange={e=>handleChange(e)}
                                 type={show ? "text" : "password"}
                                 placeholder="Enter Password"
                                 className=" px-3 py-2 rounded-md bg-purple-50 border border-purple-200 focus:outline-none focus:ring-2 focus:ring-purple-300 placeholder-gray-400 text-sm"
                             />
-                            <span className="absolute right-0 top-0" onClick={showpassword} onchange={handleChange}><img className="px-1 py-2 " width={28} src={show ? "/icons/eyecross.png" : "/icons/eye.png"} alt="eye" /></span>
+                            <span className="absolute right-0 top-0" onClick={showpassword} onChange={handleChange}><img className="px-1 py-2 " width={28} src={show ? "/icons/eyecross.png" : "/icons/eye.png"} alt="eye" /></span>
                         </div>
 
 
