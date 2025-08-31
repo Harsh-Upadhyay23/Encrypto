e3824701e190749f1fbfa7ed41d68a657d984cc2
Harsh Upadhyay <upadhyayharsh435@gmail.com>
2025-08-31 19:15:11 +0530

    done

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -3,19 +3,19 @@ import { useState } from "react";
 
 const Manager = () => {
     const [show, setShow] = useState(false);
-    const [form, setForm] = useState({site:"",username:"",password:""});
+    const [form, setForm] = useState({ site: "", username: "", password: "" });
 
-     const showpassword = () => {
-    setShow((prev) => !prev);
-  };
+    const showpassword = () => {
+        setShow((prev) => !prev);
+    };
 
-  const savePassword=()=>{
+    const savePassword = () => {
 
-  }
-    
+    }
+
     return (
         <>
-           <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>
+            <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>
 
             {/* Content */}
             <div className="flex justify-center items-center ">
@@ -32,37 +32,37 @@ const Manager = () => {
 
                     {/* Compact Form Row */}
                     <input
-                        type="text"
-                             value={form.site}
+                        type="text" onchange={handleChange}
+                        value={form.site}
                         placeholder="Enter website URL"
                         className="flex-1 px-1  rounded-md bg-blue-50 border border-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-300 placeholder-gray-400 text-sm
               w-full py-2 my-1"
                     />
                     <div className="flex gap-4 justify-center w-full">
 
-                        <input
+                        <input 
                             type="text"
-                            value={form.username}
+                            value={form.username} onchange={handleChange}
                             placeholder="Enter username"
                             className=" px-3 py-2 rounded-md bg-green-50 border border-green-200 focus:outline-none focus:ring-2 focus:ring-green-300 placeholder-gray-400 text-sm"
                         />
-                       <div className="relative ">
-                         <input
-                              value={form.password}
-                             type={show ? "text" : "password"}
-                            placeholder="Enter Password"
-                            className=" px-3 py-2 rounded-md bg-purple-50 border border-purple-200 focus:outline-none focus:ring-2 focus:ring-purple-300 placeholder-gray-400 text-sm"
-                        />
-                        <span className="absolute right-0 top-0" onClick={showpassword}><img  className="px-1 py-2 " width={28} src={show ? "/icons/eyecross.png" : "/icons/eye.png"}alt="eye" /></span>
-                       </div>
+                        <div className="relative ">
+                            <input 
+                                value={form.password} onchange={handleChange}
+                                type={show ? "text" : "password"}
+                                placeholder="Enter Password"
+                                className=" px-3 py-2 rounded-md bg-purple-50 border border-purple-200 focus:outline-none focus:ring-2 focus:ring-purple-300 placeholder-gray-400 text-sm"
+                            />
+                            <span className="absolute right-0 top-0" onClick={showpassword} onchange={handleChange}><img className="px-1 py-2 " width={28} src={show ? "/icons/eyecross.png" : "/icons/eye.png"} alt="eye" /></span>
+                        </div>
 
-                       
+
 
                         <button onClick={savePassword} className="flex rounded-full bg-green-500 px-2 py-1 justify-center items-center hover:bg-green-300">
-                             <lord-icon
-                            src="https://cdn.lordicon.com/jgnvfzqg.json"
-                            trigger="hover">
-                        </lord-icon>
+                            <lord-icon
+                                src="https://cdn.lordicon.com/jgnvfzqg.json"
+                                trigger="hover">
+                            </lord-icon>
                             Add Password
                         </button>
                     </div>
